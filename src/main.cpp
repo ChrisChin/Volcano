dbbda480e8525f262ca63166605a99e839aa611b
ChrisChin <chinchri1@myvuw.ac.nz>
2015-10-10 15:56:03 +1300

    testing example shadow map

--- src/main.cpp
+++ src/main.cpp
@@ -5,225 +5,366 @@
 
 #include "comp308.hpp"
 
-#include "imageLoader.hpp"
-#include "shaderLoader.hpp"
-
-#include "volcano.hpp"
-#include "city.hpp"
-#include "lava.hpp"
-#include "weather.hpp"
-
 using namespace std;
 using namespace comp308;
 
-// Global variables
-// 
-GLuint g_winWidth = 640;
-GLuint g_winHeight = 480;
-GLuint g_mainWindow = 0;
+//Camera & light positions
+vec3 cameraPosition(-2.5f, 3.5f, -2.5f);
+vec3 lightPosition(2.0f, 3.0f, -2.0f);
 
+//Size of shadow map
+const int shadowMapSize = 512;
 
-// Projection values
-// 
-float g_fovy = 20.0;
-float g_znear = 0.1;
-float g_zfar = 1000.0;
+//Textures
+GLuint shadowMapTexture;
 
-bool g_mouseDown = false;
-vec2 g_mousePos;
-float g_yRotation = 0;
-float g_xRotation = 0;
-float g_zoomFactor = 10;
+//window size
+int windowWidth, windowHeight;
 
-// Geometry loader and drawer
-//
-Geometry *g_geometry = nullptr;
+//Matrices
+mat4 lightProjectionMatrix, lightViewMatrix;
+mat4 cameraProjectionMatrix, cameraViewMatrix;
 
+float white[4] = { 1, 1, 1, 1 };
+float dimwhite[4] = { 0.2, 0.2, 0.2, 0.2 };
+float black[4] = { 0,0,0,0 };
 
-// Sets up where and what the light is
-// Called once on start up
-// 
-void initLight() {
-	float direction[] = { 0.0f, 0.0f, 1.0f, 0.0f };
-	float diffintensity[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-	float ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+//Called for initiation
+bool Init(void)
+{
 
-	glLightfv(GL_LIGHT0, GL_POSITION, direction);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffintensity);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
-
-	glEnable(GL_LIGHT0);
-}
-
-
-void setUpCamera() {
-	// Set up the projection matrix
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(g_fovy, float(g_winWidth) / float(g_winHeight), g_znear, g_zfar);
-
-	// Set up the view part of the model view matrix
+	//Load identity modelview
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glTranslatef(0, 0, -50 * g_zoomFactor);
-	glRotatef(g_xRotation, 1, 0, 0);
-	glRotatef(g_yRotation, 0, 1, 0);
-}
+	//Shading states
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
-
-// Draw function
-//
-void draw() {
-
-	// Set up camera every frame
-	setUpCamera();
-
-	// Black background
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// Enable flags for normal rendering
+	//Depth states
+	glClearDepth(1.0f);
+	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
+
+	glEnable(GL_CULL_FACE);
+
+	//We use glScale when drawing the scene
 	glEnable(GL_NORMALIZE);
+
+	//Create the shadow map texture
+	glGenTextures(1, &shadowMapTexture);
+	glBindTexture(GL_TEXTURE_2D, shadowMapTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, shadowMapSize, shadowMapSize, 0,
+		GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	//Use the color as the ambient and diffuse material
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
 	glEnable(GL_COLOR_MATERIAL);
 
-	// Set the current material (for all objects) to red
-	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-	
-	// Render geometry
-	g_geometry->renderGeometry();
+	//White specular material color, shininess 16
+	glMaterialfv(GL_FRONT, GL_SPECULAR, white);
+	glMaterialf(GL_FRONT, GL_SHININESS, 16.0f);
 
-	// Disable flags for cleanup (optional)
-	glDisable(GL_DEPTH_TEST);
+	//Calculate & save matrices
+	glPushMatrix();
+
+	glLoadIdentity();
+	gluPerspective(45.0f, (float)windowWidth / windowHeight, 1.0f, 100.0f);
+	glGetFloatv(GL_MODELVIEW_MATRIX, (GLfloat*)&cameraProjectionMatrix);
+
+	glLoadIdentity();
+	gluLookAt(cameraPosition.x, cameraPosition.y, cameraPosition.z,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
+	glGetFloatv(GL_MODELVIEW_MATRIX, (GLfloat*)&cameraViewMatrix);
+
+	glLoadIdentity();
+	gluPerspective(45.0f, 1.0f, 2.0f, 8.0f);
+	glGetFloatv(GL_MODELVIEW_MATRIX, (GLfloat*)&lightProjectionMatrix);
+
+	glLoadIdentity();
+	gluLookAt(lightPosition.x, lightPosition.y, lightPosition.z,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
+	glGetFloatv(GL_MODELVIEW_MATRIX, (GLfloat*)&lightViewMatrix);
+
+	glPopMatrix();
+
+
+	return true;
+}
+
+void DrawScene(float angle)
+{
+	//Display lists for objects
+	static GLuint spheresList = 0, torusList = 0, baseList = 0;
+
+	//Create spheres list if necessary
+	if (!spheresList)
+	{
+		spheresList = glGenLists(1);
+		glNewList(spheresList, GL_COMPILE);
+		{
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glPushMatrix();
+
+			glTranslatef(0.45f, 1.0f, 0.45f);
+			glutSolidSphere(0.2, 24, 24);
+
+			glTranslatef(-0.9f, 0.0f, 0.0f);
+			glutSolidSphere(0.2, 24, 24);
+
+			glTranslatef(0.0f, 0.0f, -0.9f);
+			glutSolidSphere(0.2, 24, 24);
+
+			glTranslatef(0.9f, 0.0f, 0.0f);
+			glutSolidSphere(0.2, 24, 24);
+
+			glPopMatrix();
+		}
+		glEndList();
+	}
+
+	//Create torus if necessary
+	if (!torusList)
+	{
+		torusList = glGenLists(1);
+		glNewList(torusList, GL_COMPILE);
+		{
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glPushMatrix();
+
+			glTranslatef(0.0f, 0.5f, 0.0f);
+			glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+			glutSolidTorus(0.2, 0.5, 24, 48);
+
+			glPopMatrix();
+		}
+		glEndList();
+	}
+
+	//Create base if necessary
+	if (!baseList)
+	{
+		baseList = glGenLists(1);
+		glNewList(baseList, GL_COMPILE);
+		{
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glPushMatrix();
+
+			glScalef(1.0f, 0.05f, 1.0f);
+			glutSolidCube(3.0f);
+
+			glPopMatrix();
+		}
+		glEndList();
+	}
+
+
+	//Draw objects
+	glCallList(baseList);
+	glCallList(torusList);
+
+	glPushMatrix();
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+	glCallList(spheresList);
+	glPopMatrix();
+}
+
+//Called to draw scene
+void Display(void)
+{
+	//angle of spheres in scene. Calculate from time
+	float angle = 10;
+
+
+
+	//First pass - from light's point of view
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf((GLfloat*)&lightProjectionMatrix);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixf((GLfloat*)&lightViewMatrix);
+
+	//Use viewport the same size as the shadow map
+	glViewport(0, 0, shadowMapSize, shadowMapSize);
+
+	//Draw back faces into the shadow map
+	glCullFace(GL_FRONT);
+
+	//Disable color writes, and use flat shading for speed
+	glShadeModel(GL_FLAT);
+	glColorMask(0, 0, 0, 0);
+
+	//Draw the scene
+	DrawScene(angle);
+
+	//Read the depth buffer into the shadow map texture
+	glBindTexture(GL_TEXTURE_2D, shadowMapTexture);
+	glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, shadowMapSize, shadowMapSize);
+
+	//restore states
+	glCullFace(GL_BACK);
+	glShadeModel(GL_SMOOTH);
+	glColorMask(1, 1, 1, 1);
+
+
+
+
+	//2nd pass - Draw from camera's point of view
+	glClear(GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf((GLfloat*)&cameraProjectionMatrix);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixf((GLfloat*)&cameraViewMatrix);
+
+	glViewport(0, 0, windowWidth, windowHeight);
+
+	//Use dim light to represent shadowed areas
+	glLightfv(GL_LIGHT1, GL_POSITION, (GLfloat*)&lightPosition);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, dimwhite);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, dimwhite);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, black);
+	glEnable(GL_LIGHT1);
+	glEnable(GL_LIGHTING);
+
+	DrawScene(angle);
+
+
+
+	//3rd pass
+	//Draw with bright light
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, white);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, white);
+
+	//Calculate texture matrix for projection
+	//This matrix takes us from eye space to the light's clip space
+	//It is postmultiplied by the inverse of the current view matrix when specifying texgen
+	static mat4 biasMatrix(0.5f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f);	//bias from [-1, 1] to [0, 1]
+	mat4 textureMatrix = biasMatrix*lightProjectionMatrix*lightViewMatrix;
+
+	//Set up texture coordinate generation.
+	glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
+	glTexGenfv(GL_S, GL_EYE_PLANE, (GLfloat*)&textureMatrix[0]);
+	glEnable(GL_TEXTURE_GEN_S);
+
+	glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
+	glTexGenfv(GL_T, GL_EYE_PLANE, (GLfloat*)&textureMatrix[1]);
+	glEnable(GL_TEXTURE_GEN_T);
+
+	glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
+	glTexGenfv(GL_R, GL_EYE_PLANE, (GLfloat*)&textureMatrix[2]);
+	glEnable(GL_TEXTURE_GEN_R);
+
+	glTexGeni(GL_Q, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
+	glTexGenfv(GL_Q, GL_EYE_PLANE, (GLfloat*)&textureMatrix[3]);
+	glEnable(GL_TEXTURE_GEN_Q);
+
+	//Bind & enable shadow map texture
+	glBindTexture(GL_TEXTURE_2D, shadowMapTexture);
+	glEnable(GL_TEXTURE_2D);
+
+	//Enable shadow comparison
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE_ARB, GL_COMPARE_R_TO_TEXTURE);
+
+	//Shadow comparison should be true (ie not in shadow) if r<=texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC_ARB, GL_LEQUAL);
+
+	//Shadow comparison should generate an INTENSITY result
+	glTexParameteri(GL_TEXTURE_2D, GL_DEPTH_TEXTURE_MODE_ARB, GL_INTENSITY);
+
+	//Set alpha test to discard false comparisons
+	glAlphaFunc(GL_GEQUAL, 0.99f);
+	glEnable(GL_ALPHA_TEST);
+
+	DrawScene(angle);
+
+	//Disable textures and texgen
+	glDisable(GL_TEXTURE_2D);
+
+	glDisable(GL_TEXTURE_GEN_S);
+	glDisable(GL_TEXTURE_GEN_T);
+	glDisable(GL_TEXTURE_GEN_R);
+	glDisable(GL_TEXTURE_GEN_Q);
+
+	//Restore other states
 	glDisable(GL_LIGHTING);
-	glDisable(GL_NORMALIZE);
-	glDisable(GL_COLOR_MATERIAL);
+	glDisable(GL_ALPHA_TEST);
 
-	// Move the buffer we just drew to the front
+
+	//Set matrices for ortho
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(-1.0f, 1.0f, -1.0f, 1.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+
+	//Print text
+	glRasterPos2f(-1.0f, 0.9f);
+
+	//reset matrices
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+
+	glFinish();
 	glutSwapBuffers();
-
-	// Queue the next frame to be drawn straight away
 	glutPostRedisplay();
 }
 
+//Called on window resize
+void Reshape(int w, int h)
+{
+	//Save new window size
+	windowWidth = w, windowHeight = h;
 
-// Reshape function
-// 
-void reshape(int w, int h) {
-	if (h == 0) h = 1;
-
-	g_winWidth = w;
-	g_winHeight = h;
-
-	// Sets the openGL rendering window to match the window size
-	glViewport(0, 0, g_winWidth, g_winHeight);
+	//Update the camera's projection matrix
+	glPushMatrix();
+	glLoadIdentity();
+	gluPerspective(45.0f, (float)windowWidth / windowHeight, 1.0f, 100.0f);
+	glGetFloatv(GL_MODELVIEW_MATRIX, (GLfloat*)&cameraProjectionMatrix);
+	glPopMatrix();
 }
 
-// Keyboard callback
-// Called once per button state change
-//
-void keyboardCallback(unsigned char key, int x, int y) {
-	cout << "Keyboard Callback :: key=" << key << ", x,y=(" << x << "," << y << ")" << endl;
-	// YOUR CODE GOES HERE
+//Called when a key is pressed
+void Keyboard(unsigned char key, int x, int y)
+{
+	//If escape is pressed, exit
+	if (key == 27)
+		exit(0);
+
 }
 
-
-// Special Keyboard callback
-// Called once per button state change
-//
-void specialCallback(int key, int x, int y) {
-	cout << "Special Callback :: key=" << key << ", x,y=(" << x << "," << y << ")" << endl;
-	// Not needed for this assignment, but useful to have later on
-}
-
-
-void mouseCallback(int button, int state, int x, int y) {
-	cout << "Mouse Callback :: button=" << button << ", state=" << state << ", x,y=(" << x << "," << y << ")" << endl;
-	// YOUR CODE GOES HERE
-	// ...
-	switch (button){
-
-	case 0: // left mouse button
-		g_mouseDown = (state == 0);
-		g_mousePos = vec2(x, y);
-		break;
-	case 3: // scroll foward/up
-		g_zoomFactor /= 1.1;
-		break;
-
-	case 4: // scroll back/down
-		g_zoomFactor *= 1.1;
-		break;
-	}
-}
-
-// Mouse Motion Callback function
-// Called once per frame if the mouse has moved and
-// at least one mouse button has an active state
-// 
-void mouseMotionCallback(int x, int y) {
-	cout << "Mouse Motion Callback :: x,y=(" << x << "," << y << ")" << endl;
-	// YOUR CODE GOES HERE
-	// ...
-	if (g_mouseDown) {
-		vec2 dif = vec2(x, y) - g_mousePos;
-		g_mousePos = vec2(x, y);
-		g_yRotation += 0.3 * dif.x;
-		g_xRotation += 0.3 * dif.y;
-	}
-}
-
-//Main program
-// 
-int main(int argc, char **argv) {
-	// Initialise GL, GLU and GLUT
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(640, 512);
+	glutCreateWindow("Shadow Mapping");
 
-	// Setting up the display
-	// - RGB color model + Alpha Channel = GLUT_RGBA
-	// - Double buffered = GLUT_DOUBLE
-	// - Depth buffer = GLUT_DEPTH
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	if (!Init())
+		return 0;
 
-	// Initialise window size and create window
-	glutInitWindowSize(g_winWidth, g_winHeight);
-	g_mainWindow = glutCreateWindow("COMP308 Assignment 1");
-
-
-	// Initilise GLEW
-	// must be done after creating GL context (glutCreateWindow in this case)
-	GLenum err = glewInit();
-	if (GLEW_OK != err) { // Problem: glewInit failed, something is seriously wrong.
-		cerr << "Error: " << glewGetErrorString(err) << endl;
-		abort(); // Unrecoverable error
-	}
-
-	cout << "Using OpenGL " << glGetString(GL_VERSION) << endl;
-	cout << "Using GLEW " << glewGetString(GLEW_VERSION) << endl;
-	
-	// Register functions for callback
-	glutDisplayFunc(draw);
-	glutReshapeFunc(reshape);
-
-	glutKeyboardFunc(keyboardCallback);
-	glutSpecialFunc(specialCallback);
-
-	glutMouseFunc(mouseCallback);
-	glutMotionFunc(mouseMotionCallback);
-	
-	// Create a light on the camera
-	initLight();
-
-	// Finally create our geometry
-	g_geometry = new Geometry("Volcano/res/assets/volcano.obj");
-
-	// Loop required by GLUT
-	// This will not return until we tell GLUT to finish
+	glutDisplayFunc(Display);
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
 	glutMainLoop();
-
-	// Don't forget to delete all pointers that we made
-	delete g_geometry;
 	return 0;
 }
